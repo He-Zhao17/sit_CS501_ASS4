f993dafd26b2292a2943c43ac0c149bf9f68efbe
pekokk <zh0208@163.com>
2022-03-20 22:28:59 -0400

    sas

--- src/C9E5.java
+++ src/C9E5.java
@@ -5,13 +5,13 @@ public class C9E5 {
         System.out.println("This tool is to use GregorianCalendar to output some information.\n");
         GregorianCalendar gc = new GregorianCalendar();
         System.out.println("The current year is " + gc.get(GregorianCalendar.YEAR));
-        System.out.println("The current month is " + gc.get(GregorianCalendar.MONTH));
+        System.out.println("The current month is " + (gc.get(GregorianCalendar.MONTH) + 1));
         System.out.println("The current day is " + gc.get(GregorianCalendar.DAY_OF_MONTH));
 
         System.out.println("\nNow output the year, month and the day of 1234567898765 seconds from 1/1/1970.");
         gc.setTimeInMillis(1234567898765L);
         System.out.println("The year is " + gc.get(GregorianCalendar.YEAR));
-        System.out.println("The month is " + gc.get(GregorianCalendar.MONTH));
+        System.out.println("The month is " + (gc.get(GregorianCalendar.MONTH) + 1));
         System.out.println("The day is " + gc.get(GregorianCalendar.DAY_OF_MONTH));
 
     }
